87b6f6ad3db891f17cadf9a123e7c5a087a6c5d2
zoya-kk <zoya.uet@gmail.com>
2026-08-17 12:41:58 -0400

    Fix App routing and add profile route

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import MainPage from './components/MainPage/MainPage';
 import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
 import DetailsPage from './components/DetailsPage/DetailsPage';
 import SearchPage from './components/SearchPage/SearchPage';
+import Profile from './components/Profile/Profile';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 
 function App() {
-    const navigate = useNavigate();
-
     return (
         <>
             <Navbar />
+
             <Routes>
                 <Route path="/" element={<MainPage />} />
                 <Route path="/app" element={<MainPage />} />
@@ -22,6 +22,7 @@ function App() {
                 <Route path="/app/register" element={<RegisterPage />} />
                 <Route path="/app/product/:productId" element={<DetailsPage />} />
                 <Route path="/app/search" element={<SearchPage />} />
+                <Route path="/app/profile" element={<Profile />} />
             </Routes>
         </>
     );
